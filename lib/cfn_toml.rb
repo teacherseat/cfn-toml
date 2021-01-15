95c757cefea624e43128e3e739d824d24b65431c
Andrew Brown <andrew@exampro.co>
2021-01-15 12:13:37 -0500

    cfn-toml init

--- lib/cfn_toml.rb
+++ lib/cfn_toml.rb
@@ -1,6 +1,24 @@
 require 'toml-rb'
+require 'fileutils'
 
 module CfnToml
+  def self.init toml_filepath, stackname, profile
+    region = `aws configure get region --profile #{profile}`
+    region ||= 'us-east-1'
+    stackname ||= 'stackname'
+
+    toml_path = File.dirname toml_filepath
+    FileUtils.mkpath(toml_path) unless File.exists?(toml_path)
+
+    File.open(toml_filepath, "w") do |f| 
+      f.write "[deploy]\n"
+      f.write "profile = '#{profile}'\n"
+      f.write "stack_name = '#{stackname}'\n"
+      f.write "region = '#{region.chomp}'\n\n"
+      f.write "[parameters]\n"
+    end
+  end
+
   def self.key toml_filepath, key
     data = TomlRB.load_file(toml_filepath)
     namespace, key = key.split('.')
--- lib/cfn_toml/version.rb
+++ lib/cfn_toml/version.rb
@@ -1,3 +1,3 @@
 module CfnToml
-  VERSION = '1.0.3'
+  VERSION = '1.0.4'
 end
